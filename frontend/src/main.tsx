b85735d82562c05541547a209cffa1225fcb8795
XOMRKOB <ru89050995432@yandex.ru>
2023-10-25 15:23:13 +0600

    add tailwind css

---
+++ frontend/src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import './style.css'
+import {Login} from "./app/pages";
+
+
+const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: <Login/>
+    },
+  ],{
+    basename: "/"
+  }
+)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
