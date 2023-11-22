2054eac5f597cacdd1699e62ac86ea6894773ba9
XOMRKOB <ru89050995432@yandex.ru>
2023-11-22 09:57:05 +0600

    refactoring ArticleUrls.tsx

--- frontend/src/app/components/article_urls/ArticleUrls.tsx
+++ frontend/src/app/components/article_urls/ArticleUrls.tsx
@@ -5,14 +5,22 @@ import {RootState} from "../../store";
 import {ArticleUrlsNoExpandedNoUrls} from "./ArticleUrlsNoExpandedNoUrls.tsx";
 import {ArticleUrlsNoExpandedWithUrls} from "./ArticleUrlsNoExpandedWithUrls.tsx";
 import {ArticleUrlsExpanded} from "./ArticleUrlsExpanded.tsx";
+import {useUpdateArticleMutation} from "../../services/backend";
+import {useDebounce} from "@consta/uikit/useDebounce";
 
 export const ArticleUrls = () => {
-  const urls = useSelector((state: RootState) => state.articles.current_article?.urls)
+  const article_id = useSelector((state: RootState) => state.articles.current_article?.id)
+  const storeUrls = useSelector((state: RootState) => state.articles.current_article?.urls)
   const [isExpanded, setIsExpanded] = useState<boolean>(false)
   const expandedRef = useRef<HTMLDivElement>(null)
   const datePickerRef = useRef<HTMLDivElement>(null)
+  const [updateArticle] = useUpdateArticleMutation()
+  const debounceUpdateArticle = useDebounce(updateArticle, 300)
+  const [dateAccessed, setDateAccessed] = useState<Date | null>(new Date())
+  const [urls, setUrls] = useState<(string | null)[]>([null])
 
   const handleClickOutside = (e: TouchEvent | MouseEvent) => {
+    e.preventDefault()
     console.log(e.target)
     // if (datePickerRef.current && !datePickerRef.current.contains(e.target as Node)) {return;}
     if (expandedRef.current) {
@@ -27,15 +35,39 @@ export const ArticleUrls = () => {
     return () => document.removeEventListener('mousedown', handleClickOutside);
   });
 
+  useEffect(() => {
+    // Первоначальная установка состояния из бэка
+    if (storeUrls?.date_accessed) {setDateAccessed(new Date(storeUrls.date_accessed))}
+    if (storeUrls?.urls) {if (storeUrls.urls.length > 0) setUrls(storeUrls.urls)}
+  }, [storeUrls])
+
+  useEffect(() => {
+    console.log(isExpanded, dateAccessed, urls)
+    if (!isExpanded && dateAccessed && urls[0] != null) {
+      debounceUpdateArticle({
+        id: article_id, urls: {
+          date_accessed: dateAccessed,
+          urls: urls
+        }
+      })
+    }
+  }, [isExpanded])
+
   return (
     <div>
-      <div className={'ms-1 text-zinc-500'}>URLs</div>
+      <span className={'ms-1 text-zinc-500'}>URLs</span>
       {isExpanded ?
-        <ArticleUrlsExpanded expandedRef={expandedRef} datePickerRef={datePickerRef}/> :
+        <ArticleUrlsExpanded expandedRef={expandedRef}
+                             datePickerRef={datePickerRef}
+                             dateAccessed={dateAccessed}
+                             setDateAccessed={setDateAccessed}
+                             urls={urls}
+                             setUrls={setUrls}
+        /> :
         <>
-          {urls && urls.urls.length > 0 ?
-            <ArticleUrlsNoExpandedWithUrls setIsExpanded={setIsExpanded} /> :
-            <ArticleUrlsNoExpandedNoUrls setIsExpanded={setIsExpanded} />
+          {storeUrls && storeUrls.urls.length > 0 ?
+            <ArticleUrlsNoExpandedWithUrls setIsExpanded={setIsExpanded}/> :
+            <ArticleUrlsNoExpandedNoUrls setIsExpanded={setIsExpanded}/>
           }
         </>
       }
--- frontend/src/app/components/article_urls/ArticleUrlsExpanded.tsx
+++ frontend/src/app/components/article_urls/ArticleUrlsExpanded.tsx
@@ -1,56 +1,60 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {DatePicker} from "@consta/uikit/DatePicker";
 import {TextField} from "@consta/uikit/TextField";
-import {useSelector} from "react-redux";
-import {RootState} from "../../store";
 import {Button} from "@consta/uikit/Button";
 import {IconAdd} from "@consta/uikit/IconAdd";
 import {IconClose} from "@consta/uikit/IconClose";
 
+
 interface ArticleUrlsExpandedPropsIFace {
   expandedRef : React.RefObject<HTMLDivElement>,
   datePickerRef : React.RefObject<HTMLDivElement>
+  dateAccessed: Date | null,
+  setDateAccessed: React.Dispatch<React.SetStateAction<Date | null>>,
+  urls: (string | null)[],
+  setUrls: React.Dispatch<React.SetStateAction<(string | null)[]>>
 }
 
-export const ArticleUrlsExpanded = ({expandedRef, datePickerRef}: ArticleUrlsExpandedPropsIFace) => {
-  const currentArticleURLs = useSelector((state: RootState) => state.articles.current_article?.urls)
-  const [dateAccessed, setDateAccessed] = useState<Date | null>(null)
-  const [urls, setUrls] = useState<(string | null)[]>([null])
+export const ArticleUrlsExpanded = (props: ArticleUrlsExpandedPropsIFace) => {
+  const {
+    expandedRef,
+    datePickerRef,
+    dateAccessed,
+    setDateAccessed,
+    urls,
+    setUrls
+  } = props
   const now = new Date()
 
   const handleClickAddURL = () => {
+    // Добавляет строчку с пустым URL
     setUrls(prev => [...prev, null])
   }
 
-  const handleClickDeleteURL = () => {
-    setUrls(prev => prev.splice(prev.length - 1 , 1))
+  const handleClickDeleteURL = (index: number) => {
+    // Удаляет строчку с URL
+    const mutateUrls = urls.filter((item, i) => {
+      if (i !== index) return item
+    })
+    setUrls(mutateUrls)
   }
 
-  useEffect(() => {}, [dateAccessed])
-
-  useEffect(() => {
-    if (currentArticleURLs?.date_accessed) {
-      setDateAccessed(new Date(currentArticleURLs?.date_accessed))
-    }
-    if (currentArticleURLs?.urls) {
-      if (currentArticleURLs.urls.length > 0)
-      setUrls(currentArticleURLs.urls)
-    }
-
-  }, [currentArticleURLs])
 
   const handleChangeUrl = (value: string | null, index: number) => {
-    console.log(urls)
-    setUrls( prev => {
-      prev[index] = prev[index] + value
-      return prev
+    // Обновление локального состояния URLs
+    const mutateUrls = urls.map((url, i) => {
+      if (i === index) {
+        return value
+      } else {
+        return url
+      }
     })
-    console.log(urls)
+    setUrls(mutateUrls)
   }
-  // TODO: Не работает URLS
+
   return (
     <div ref={expandedRef} className={"border border-sky-700 rounded p-4"}>
-      <DatePicker onChange={({ value }) => setDateAccessed(value)}
+      <DatePicker onChange={({value}) => setDateAccessed(value)}
                   ignoreOutsideClicksRefs={[expandedRef]}
                   label={'Дата доступа'}
                   maxDate={now}
@@ -59,23 +63,25 @@ export const ArticleUrlsExpanded = ({expandedRef, datePickerRef}: ArticleUrlsExp
                   width={'full'}
                   size={'s'}
       />
-      {urls.map( (url, index) => (
+      {urls.map((url, index) => (
         <div key={index} className={'relative'}>
-          <TextField onChange={({ value }: { value: string | null }) => handleChangeUrl(value, index)}
+          <TextField onChange={({value}: { value: string | null }) => handleChangeUrl(value, index)}
                      className={'my-2'}
                      size={'s'}
-                     label={`${index+1}. URL`}
+                     label={`${index + 1}. URL`}
                      value={url}
                      width={'full'}
           />
           {index !== 0 &&
               <div className={'absolute top-0 right-0 p-2'}>
-                <Button onlyIcon iconLeft={IconClose} view={'clear'} form={'round'} size={'xs'} onClick={handleClickDeleteURL}/>
+                  <Button onlyIcon iconLeft={IconClose} view={'clear'} form={'round'} size={'xs'}
+                          onClick={() => handleClickDeleteURL(index)}/>
               </div>
           }
         </div>
       ))}
-      <Button className={'mt-2'} label={'Добавить еще один URL'} view={'clear'} size={'xs'} iconLeft={IconAdd} onClick={handleClickAddURL}/>
+      <Button className={'mt-2'} label={'Добавить еще один URL'} view={'clear'} size={'xs'} iconLeft={IconAdd}
+              onClick={handleClickAddURL}/>
     </div>
   )
 }
