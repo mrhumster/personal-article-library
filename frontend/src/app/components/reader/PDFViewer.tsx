3fb5afb6f1c06f561e6063f0fe25cb0c769b9b8c
XOMRKOB <ru89050995432@yandex.ru>
2024-02-05 18:48:50 +0600

    start tuning pdf viewer

--- frontend/src/app/components/reader/PDFViewer.tsx
+++ frontend/src/app/components/reader/PDFViewer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import {RootState} from "../../store";
-import { Viewer, Worker, LocalizationMap } from '@react-pdf-viewer/core'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import {LocalizationMap, SpecialZoomLevel, Viewer, Worker} from '@react-pdf-viewer/core'
+import {defaultLayoutPlugin} from '@react-pdf-viewer/default-layout'
 import ru_RU from '@react-pdf-viewer/locales/lib/ru_RU.json'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
@@ -12,6 +12,12 @@ export const PDFViewer = () => {
   const file = useSelector((state: RootState) => state.ui.reader.activeTab)
   // const { data } = useGetFileQuery(file?.id, {skip: !file})
   const newPlugin = defaultLayoutPlugin()
+
+  const handlePageChange = ({currentPage, doc}: {currentPage: number, doc: object}) => {
+    console.log(currentPage)
+    console.log(doc)
+  }
+
   return (
     <div className='pdf-container'>
     <Worker workerUrl={`//unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
@@ -20,6 +26,9 @@ export const PDFViewer = () => {
                   localization={ru_RU as unknown as LocalizationMap}
                   plugins={[newPlugin]}
                   theme={'dark'}
+                  initialPage={10}
+                  defaultScale={SpecialZoomLevel.PageFit}
+                  onPageChange={handlePageChange}
           />
       }
     </Worker>
